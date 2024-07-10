d202eb1a706bb55f065f63747c10659f0e24c49f
Swager0932 <swagerbot2000@gmail.com>
2024-07-10 00:19:38 -0400

    Fixed run error pt3

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client"
 import App from "./App.jsx"
 import 'bootstrap/dist/css/bootstrap.min.css'
 import "./index.css"
-import "./app.css"
+import "./App.css"
 import { BrowserRouter } from "react-router-dom"
 
 ReactDOM.createRoot(document.getElementById("root")).render(
